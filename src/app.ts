bc0bd6b046996282d1012b3da504199d8e7003c0
quocbao_home <quocbao.tn@gmail.com>
2018-01-25 15:42:23 +0700

    load Themes OK w/o js

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,52 @@
+import {Aurelia} from 'aurelia-framework';
+import {Router, RouterConfiguration} from 'aurelia-router';
+import $ from 'jquery';
+
 export class App {
-  message = 'Hello World!';
+
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'EasyWeb';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: './modules/common/welcome',      nav: true, title: 'Welcome' },
+      // { route: 'users',         name: 'users',        moduleId: PLATFORM.moduleName('./modules/users/users'),        nav: true, title: 'Github Users' },
+      { route: 'sample',         name: 'sample',        moduleId: './modules/samples/sample',        nav: true, title: 'Case Study' },
+      // { route: 'child-router',  name: 'child-router', moduleId: PLATFORM.moduleName('./modules/common/child-router'), nav: true, title: 'Child Router' },
+    ]);
+
+    this.router = router;
+    
+  }
+  attached() {
+    // var script = document.createElement("script");
+    // script.src = "assets/vendors/base/vendors.bundle.js";
+    // script.type = "text/javascript";
+    // document.getElementsByTagName("body")[0].appendChild(script);
+
+    // var s2 = document.createElement("script");
+    // s2.src = "assets/demo/default/base/scripts.bundle.js";
+    // s2.type = "text/javascript";
+    // document.getElementsByTagName("body")[0].appendChild(s2);
+    
+    // var s3 = document.createElement("script");
+    // s3.src = "assets/vendors/custom/fullcalendar/fullcalendar.bundle.js";
+    // s3.type = "text/javascript";
+    // document.getElementsByTagName("body")[0].appendChild(s3);
+    
+    // var s4 = document.createElement("script");
+    // s4.src = "assets/app/js/dashboard.js";
+    // s4.type = "text/javascript";
+    // document.getElementsByTagName("body")[0].appendChild(s4);
+    
+      // <!--begin::Base Scripts -->
+      // <script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+      // <script src="assets/demo/demo2/base/scripts.bundle.js" type="text/javascript"></script>
+      // <!--end::Base Scripts -->
+      // <!--begin::Page Vendors -->
+      // <script src="assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+      // <!--end::Page Vendors -->
+      // <!--begin::Page Snippets -->
+      // <script src="assets/app/js/dashboard.js" type="text/javascript"></script>
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
+import * as jquery from 'jquery';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
+    .plugin('jquery')
     .standardConfiguration()
     .feature('resources');
 
@@ -13,6 +15,6 @@ export function configure(aurelia: Aurelia) {
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
   }
-
+  //require.config({ paths: { 'vs': '../node_modules/monaco-editor/dev/vs' }});
   aurelia.start().then(() => aurelia.setRoot());
 }
